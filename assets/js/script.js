9ff37226cc4eff0e01d1f7dcc685cd10104de627
bulk <harryhamlin@gmail.com>
2022-12-15 18:20:50 -0800

    added a number of error checkers and a button to clear local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,5 @@
 // todo:
-// write clearing funtions for everything (field clears on search)
-// write an error message for a 400
 // write a function that checks for duplicates in search history
-// write a function that clears search button history
 // annotate all weather outputs
 // clean up .append functions?
 
@@ -13,18 +10,18 @@ $(function () {
 
     let lat;
     let lon;
+    let currentPrintArray;
+    let printArrayLength;
     buttonPrint();
-    let controller = new AbortController();
-    let signal = controller.signal;
 
     // event listener for the submit button executing the funciton to utilize the api to pull weather data
     $('#submit-button').on('click', function (event) {
         let cityName = $('#city-name').val();
         if (cityName) {
             $('#city-name').val('');
+            clearEverything();
             storeSearch(cityName);
             grabInfo(cityName);
-            buttonPrint();
         }
     })
 
@@ -34,11 +31,16 @@ $(function () {
         if (userClick === 'BUTTON') {
             if (cityName) {
                 $('#city-name').val('');
+                clearEverything();
                 storeSearch(cityName);
                 grabInfo(cityName);
-                buttonPrint();
             }
         }
+    })
+
+    $('#clear-button').on('click', function (event) {
+        window.localStorage.clear('search-history');
+        clearEverything();
     })
 
     // this function executes multiple apis, first one to grab lat and lon of specified city, the next to pull the weather forecast. from within the second fetch, the dataParse function is executed
@@ -46,10 +48,16 @@ $(function () {
         let geoLocate = ('http://api.openweathermap.org/geo/1.0/direct?q=' + cityName + '&limit=5&appid=8608b342c629b737fa132244a51c05e7');
         fetch(geoLocate)
             .then(function (response) {
-                return response.json();
+                if (response.status !== 200) {
+                    window.alert('Query returned status: ' + response.status)
+                } else {
+                    return response.json();
+                }
             })
             .then(function (data) {
                 if (data.length === 0) {
+                    window.alert('Query failed to return a result')
+                    return Promise.reject('Query failed to return a result')
                 } else {
                     lat = data[0].lat;
                     lon = data[0].lon;
@@ -61,6 +69,7 @@ $(function () {
                 return response.json();
             })
             .then(function (data) {
+
                 let forecastArray = [];
                 for (let i = 2; i < 35; i = i + 8) {
                     let date = dayjs(dayjs.unix(data.list[i].dt)).format('MMM-DD-YY');
@@ -72,7 +81,7 @@ $(function () {
                     forecastArray.push(singleDayArray);
                 }
                 for (let i = 0; i < 5; i++) {
-                    $('#forecast').append('<div class="card m-2 col bg-dark text-light"><div class="card-body"><h5 class="card-title" id="date">' + forecastArray[i][0] + '</h5><i id="icon">' + forecastArray[i][1] + '</i><h6 class="card-subtitle mb-2 text-muted" id="temp">' + forecastArray[i][2] + '</h6><h6 class="card-subtitle mb-2 text-muted" id="wind">' + forecastArray[i][3] + '</h6><h6 class="card-subtitle mb-2 text-muted" id="humidity">' + forecastArray[i][4] + '</h6></div></div>')
+                    $('#forecast').append('<div class="card m-2 col-lg col-sm-12 bg-dark text-light" id="forecast-card"><div class="card-body"><h5 class="card-title" id="date">' + forecastArray[i][0] + '</h5><i id="icon">' + forecastArray[i][1] + '</i><h6 class="card-subtitle mb-2 text-muted" id="temp">' + forecastArray[i][2] + '</h6><h6 class="card-subtitle mb-2 text-muted" id="wind">' + forecastArray[i][3] + '</h6><h6 class="card-subtitle mb-2 text-muted" id="humidity">' + forecastArray[i][4] + '</h6></div></div>')
                 }
                 let forecastFetchUrl = 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&units=imperial&appid=8608b342c629b737fa132244a51c05e7';
                 return fetch(forecastFetchUrl);
@@ -87,22 +96,39 @@ $(function () {
                 let temp = data.main.temp;
                 let wind = data.wind.speed;
                 let humidity = data.main.humidity;
-                $('#current-forecast').append('<div class="card-body"><h5 class="card-title" id="city-current">' + name + date + icon + '</h5><h6 class="card-subtitle mb-2 text-muted" id="temp-current">' + temp + '</h6><h6 class="card-subtitle mb-2 text-muted" id="wind-current">' + wind + '</h6><h6 class="card-subtitle mb-2 text-muted" id="humidity-current">' + humidity + '</h6></div>')
+                $('#current-forecast').append('<div class="card-body" id="weather-card"><h5 class="card-title" id="city-current">' + name + date + icon + '</h5><h6 class="card-subtitle mb-2 text-muted" id="temp-current">' + temp + '</h6><h6 class="card-subtitle mb-2 text-muted" id="wind-current">' + wind + '</h6><h6 class="card-subtitle mb-2 text-muted" id="humidity-current">' + humidity + '</h6></div>');
+                buttonPrint();
             })
+    }
+
+    function clearEverything() {
+        for (let i = 0; i < 5; i++) {
+            $('#forecast-card').remove();
+        }
+        $('#weather-card').remove();
+        for (let i = 0; i < printArrayLength; i++) {
+            $('#history-search-button').remove();
+        }
     }
 
     function storeSearch(cityName) {
         let searchHistory = JSON.parse(localStorage.getItem("search-history"));
         if (searchHistory === null) {
-            searchHisotry = [];
+            searchHistory = [cityName];
+            console.log(searchHistory);
+        } else {
+            searchHistory.unshift(cityName);
+
         }
-        searchHistory.push(cityName);
         localStorage.setItem("search-history", JSON.stringify(searchHistory));
     }
 
     function buttonPrint() {
-        let currentPrintArray = JSON.parse(localStorage.getItem("search-history"));
-        let printArrayLength = currentPrintArray.length;
+        currentPrintArray = JSON.parse(localStorage.getItem("search-history"));
+        if (currentPrintArray === null) {
+            currentPrintArray = [];
+        }
+        printArrayLength = currentPrintArray.length;
         if (currentPrintArray.length >= 10) {
             printArrayLength = 10
         }
